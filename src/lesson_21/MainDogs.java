ec2797027a3c3cac0886b9f7b552942459ca7ee4
SergeyB <sergiibugaienko@gmail.com>
2025-02-25 12:58:30 +0100

    lesson 21 add

---
+++ src/lesson_21/MainDogs.java
@@ -0,0 +1,26 @@
+package lesson_21;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 25.02.2025
+ */
+
+public class MainDogs {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Bim", 5);
+
+        dog.info();
+
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+
+        dog.info();
+
+    }
+}
