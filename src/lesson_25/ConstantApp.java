1577775da066d382c25f2f931027114add08d931
SergeyB <sergiibugaienko@gmail.com>
2025-03-03 12:50:36 +0100

    lesson 25 add

---
+++ src/lesson_25/ConstantApp.java
@@ -0,0 +1,26 @@
+package lesson_25;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 03.03.2025
+ */
+
+public class ConstantApp {
+    public static void main(String[] args) {
+
+        // Невозможно изменить значение константы
+        // ConstantDemo.PI = 555;
+
+        System.out.println(ConstantDemo.PI);
+
+        System.out.println(ConstantDemo.doublePiSqrt());
+
+        String result = "Java" + " fun!";
+
+
+        StringBuilder sb = new StringBuilder();
+        // TODO показать на след лекции
+
+
+    }
+}
