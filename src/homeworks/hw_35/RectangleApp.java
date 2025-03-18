4b6b955afa3eaf733c8c4168d6470e3054882104
SergeyB <sergiibugaienko@gmail.com>
2025-03-18 18:15:04 +0100

    lesson 35 add

---
+++ src/homeworks/hw_35/RectangleApp.java
@@ -0,0 +1,19 @@
+package homeworks.hw_35;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 18.03.2025
+ */
+
+public class RectangleApp {
+
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle(5.0, 3.0);
+        Rectangle rect2 = new Rectangle(5.0, 3.0);
+        Rectangle rect3 = new Rectangle(4.0, 2.0);
+
+        System.out.println(rect1.equals(rect2)); // Должно быть true
+        System.out.println(rect1.equals(rect3)); // Должно быть false
+    }
+}
+
