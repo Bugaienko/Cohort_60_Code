cd3937d3e64b75cf6ac54039e3fe09bb2e23d5aa
SergeyB <sergiibugaienko@gmail.com>
2025-03-26 13:00:09 +0100

    lesson 42 add

---
+++ src/lesson_42/CarSpeedComparator.java
@@ -0,0 +1,16 @@
+package lesson_42;
+
+import java.util.Comparator;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 26.03.2025
+ */
+
+public class CarSpeedComparator implements Comparator<Car> {
+
+    @Override
+    public int compare(Car car1, Car car2) {
+        return car1.getMaxSpeed() - car2.getMaxSpeed();
+    }
+}
