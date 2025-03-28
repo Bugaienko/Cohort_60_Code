fa645f6850b7eff693f15771a3b0293d104c629b
SergeyB <sergiibugaienko@gmail.com>
2025-03-28 13:11:34 +0100

    lesson 44 add

---
+++ src/consultations/cons_18/LinkedExample.java
@@ -0,0 +1,38 @@
+package consultations.cons_18;
+
+import lists.MyLinkedList;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 27.03.2025
+ */
+
+public class LinkedExample {
+    public static void main(String[] args) {
+
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        list.add(70);
+        list.add(80);
+        list.add(90);
+
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+        int value = list.get(4);
+        System.out.println(value);
+
+
+
+
+
+
+    }
+}
