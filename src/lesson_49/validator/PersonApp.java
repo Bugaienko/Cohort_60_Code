c3d58e1230f91e1cddf37773dfa52dfba1977119
SergeyB <sergiibugaienko@gmail.com>
2025-04-04 12:38:23 +0200

    lesson 49 add

---
+++ src/lesson_49/validator/PersonApp.java
@@ -0,0 +1,19 @@
+package lesson_49.validator;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 04.04.2025
+ */
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Person person = new Person("john@gmail.com", "qwerty");
+
+        System.out.println(person);
+
+        System.out.println("=============\n");
+
+        Person person1 = new Person("john.gmail.com", "qwerty");
+        System.out.println(person1);
+    }
+}
